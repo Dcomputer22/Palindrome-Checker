c5782703197da60b67ce3dc8a400995af0c9531d
Fatima Jimoh <fatimaoyiza18@gmail.com>
2025-05-09 23:54:40 +0100

    Remove console and restructure if else to clear bugs

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const checkForPalindrome = () => {
   let textInput = document.getElementById('text-input');
-  let value = textInput.value.trim();
-
-  if (value === '') {
-    alert('Please input a value');
-  }
-
-  let normalisedText = value.replace(/[ _,.:/\-\(\)]/g, '').toLowerCase();
-  console.log('normalisedText', normalisedText);
-
+  let text = textInput.value;
+  let normalisedText = text.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
   let reversedText = normalisedText.split('').reverse().join('');
-  console.log('reversedText', reversedText);
 
   let resultElement = document.getElementById('result');
 
-  if (normalisedText === reversedText) {
-    resultElement.innerText = `${value} is a palindrome`;
+  if (normalisedText === '') {
+    alert('Please input a value');
+  } else if (normalisedText.length === 1) {
+    resultElement.innerText = `${text} is a palindrome`;
+  } else if (normalisedText === reversedText) {
+    resultElement.innerText = `${text} is a palindrome`;
   } else {
-    resultElement.innerText = `${value} is not a palindrome`;
+    resultElement.innerText = `${text} is not a palindrome`;
   }
 };
